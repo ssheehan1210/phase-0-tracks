f86d14d8f853507f8716e15c8bfa89956269cfe7
Dennis Samuel Sheehan <ssheehan1210@yahoo.com>
2017-08-07 05:00:56 -0500

    add puppy class file

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# CLASS --------------------------------------------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------------------------------------------
+# Ear length: varies
+# Body size: varies
+# Color: varies
+# Name: varies
+# Friendly: yes
+# Leg count: 4
+# Eye count: 2
+
+# BEHAVIOR -----------------------------------------------------------
+# Sit
+# Bark
+# Roll over
+# Run
+# Stay
+# Hug
